8bfc7640b7bddb0598002dde5e8bb693ec41aca2
Aditya Dey <deyaditya2004@gmail.com>
2025-02-16 12:46:32 +0530

    Modified

--- TwoPointers/q1PairSum.cpp
+++ TwoPointers/q1PairSum.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 void findPairSum(const vector<int>& nums, int target) {
-    unordered_map<int, int> mp;
+    unordered_map<int, int> map;
+    vector<pair<int, int>> pairs;
 
-    for(int num : nums) {
+    // Find pairs and store them in the vector
+    for (int num : nums) {
         int complement = target - num;
 
-        if(mp.find(complement) != mp.end()) {
-            cout << "Pair: (" << num <<", " << complement << ")\n";
+        if (map.find(complement) != map.end()) {
+            // Always store the smaller number first for consistency
+            pairs.push_back({min(num, complement), max(num, complement)});
         }
+        map[num]++;
+    }
 
-        // Add the current number to the map
-        mp[num++];
+    // Sort the pairs by the first element (ascending order)
+    sort(pairs.begin(), pairs.end());
+
+    // Print the sorted pairs
+    for (auto& pair : pairs) {
+        cout << "Pair found: (" << pair.first << ", " << pair.second << ")\n";
     }
 }
 
